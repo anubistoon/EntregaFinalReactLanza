5e6ec148f8f358e0edfd982c1a7f5e0ff718a8d6
Willington Lanza <wdlc2003@homail.com>
2024-12-12 19:59:56 -0300

    Tercera subida2

--- src/main.jsx
+++ src/main.jsx
@@ -9,12 +9,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDrTGYljxgMhN3jPfMerJ2F-U55RYG_J7g",
-  authDomain: "react-xiaomi.firebaseapp.com",
-  projectId: "react-xiaomi",
-  storageBucket: "react-xiaomi.firebasestorage.app",
-  messagingSenderId: "223563423666",
-  appId: "1:223563423666:web:9df4da447d0935be94ceab"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
